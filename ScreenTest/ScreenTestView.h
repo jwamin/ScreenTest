4d837b6a22d7caae001aa51fa5aa6bfcc9c49edc
Joss Manger <Jwam3rd@gmail.com>
2019-02-18 16:45:27 -0500

    Initial Commit

---
+++ ScreenTest/ScreenTestView.h
@@ -0,0 +1,13 @@
+//
+//  ScreenTestView.h
+//  ScreenTest
+//
+//  Created by Joss Manger on 2/18/19.
+//  Copyright © 2019 Joss Manger. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ScreenTestView : ScreenSaverView
+
+@end
---
+++ ScreenTest/ScreenTestView.m
@@ -0,0 +1,52 @@
+//
+//  ScreenTestView.m
+//  ScreenTest
+//
+//  Created by Joss Manger on 2/18/19.
+//  Copyright © 2019 Joss Manger. All rights reserved.
+//
+
+#import "ScreenTestView.h"
+
+@implementation ScreenTestView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
